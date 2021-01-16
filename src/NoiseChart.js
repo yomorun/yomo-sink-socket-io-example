82a84dfea52d7cdf3594aeaaab57c1c1a06376f8
Ya Zhuang <zhuangya@users.noreply.github.com>
2021-01-16 15:38:31 +0800

    refactor: rewrite with rx

---
+++ src/NoiseChart.js
@@ -0,0 +1,21 @@
+import { useObservableState } from "observable-hooks";
+import { Main, Logo, Num } from "./ui";
+import cx from "classnames";
+
+import { useLastReading } from "./NoiseProvider";
+
+export default function NoiseChart() {
+  const lastReading$ = useLastReading();
+
+  const lr = useObservableState(lastReading$, () => ["n/a", "n/a"]);
+
+  return (
+    <Main>
+      <Logo className='logo' src='logo.png' alt='YoMo' />
+    <p>
+      实时噪音分贝值：
+      <Num className={cx({ glow: lr[0] !== lr[1] })}>{lr[1]}</Num>
+    </p>
+    </Main>
+  );
+}
---
+++ src/NoiseProvider.js
@@ -0,0 +1,40 @@
+import io from "socket.io-client";
+
+import { createContext, useContext, useState, useEffect } from "react";
+
+import { EMPTY, fromEvent } from "rxjs";
+import { pairwise, map } from "rxjs/operators";
+
+const NoiseContext = createContext({});
+
+export function NoiseProvider({ children }) {
+  const [raw$, setRaw$] = useState(EMPTY);
+
+  useEffect(() => {
+    const socket = io("https://yomo.cel-la.store", {
+      transports: ["websocket"],
+    });
+
+    const raw$ = fromEvent(socket, "receive_sink").pipe(
+      map(x => x.toFixed(1)),
+      pairwise(),
+    );
+    setRaw$(raw$);
+
+    return () => {
+      setRaw$(null);
+      return socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <NoiseContext.Provider value={{ lastReading$: raw$ }}>
+      {children}
+    </NoiseContext.Provider>
+  );
+}
+
+export function useLastReading() {
+  const { lastReading$ } = useContext(NoiseContext);
+  return lastReading$;
+}
